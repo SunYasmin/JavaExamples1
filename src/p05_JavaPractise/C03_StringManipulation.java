201888389b9649eb7281f4cdcdc8874c872a9b51
SunYasmin <110902506+SunYasmin@users.noreply.github.com>
2022-09-26 09:08:47 +0300

    Update C03_StringManipulation.java

--- src/p05_JavaPractise/C03_StringManipulation.java
+++ src/p05_JavaPractise/C03_StringManipulation.java
@@ -1,2 +1,25 @@
 package p05_JavaPractise;public class C03_StringManipulation {
+  
+  public static void main(String[] args) {
+
+        // Soru 1) String methodlarini kullanarak
+        // “  Java ogrenmek123 Cok guzel@  ”  String’ini
+        // “Java ogrenmek cok guzel.” sekline getirin.
+
+        String str = "  Java ogrenmek123 Cok guzel@ ";
+
+        str =str.trim();   //trimle bosluktan kurtuldum "Java ogrenmek123 Cok guzel" oldu
+
+        str = str.replaceAll("\\d", ""); //Java ogrenmek Cok guzel@
+        str = str.replaceAll("@",""); //Java ogrenmek Cok guzel
+        str = str.replaceAll("C", "c"); //Java ogrenmek c
+        str = str+"."; //Java ogrenmek cok guzel.
+
+        System.out.println(str); //Java ogrenmek cok guzel.
+
+
+
+    }
+  
+  
 }
