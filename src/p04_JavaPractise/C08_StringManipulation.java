fa51166d91bfc3db149118beedd5f2761e18d054
SunYasmin <110902506+SunYasmin@users.noreply.github.com>
2022-09-26 09:06:23 +0300

    Update C08_StringManipulation.java

--- src/p04_JavaPractise/C08_StringManipulation.java
+++ src/p04_JavaPractise/C08_StringManipulation.java
@@ -1,2 +1,19 @@
 package p04_JavaPractise;public class C08_StringManipulation {
+  /*
+        Kullanicidan bir kelime girmesini isteyin.
+        * Sozcukte tek sayida karakter ve 3 veya daha fazla karakter iceriyorsa,
+        * kelimenin ortasindaki karakteri yazdirin.
+        */
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        System.out.println("bir kelime giriniz :");
+        String kelime = scan.next();
+        char ortancaKrk = kelime.charAt((kelime.length()-1)/2);
+        if(kelime.length()%2==1 && kelime.length()>=3){
+            System.out.println("ortanca karakater :" + ortancaKrk);
+        }
+    }
+
+  
+  
 }
