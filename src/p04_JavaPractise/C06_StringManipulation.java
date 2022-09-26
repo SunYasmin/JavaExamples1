376cd92bdf88cad8d28e8eda37f7e17f8cae1444
SunYasmin <110902506+SunYasmin@users.noreply.github.com>
2022-09-26 09:05:23 +0300

    Update C06_StringManipulation.java

--- src/p04_JavaPractise/C06_StringManipulation.java
+++ src/p04_JavaPractise/C06_StringManipulation.java
@@ -1,2 +1,29 @@
 package p04_JavaPractise;public class C06_StringManipulation {
+  
+   public static void main(String[] args) {
+        /*
+         * girilen String icinde "xyz" dizimi var ise true degilse false return eden metod yaziniz
+         *
+         * input      output
+         * axyzm  ==   true
+         * xyz    ==   true
+         * x.yz   ==   false
+         * xyaz   ==   false
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("bir string giriniz :");
+        String str = scan.nextLine().toLowerCase();
+        System.out.println(xyzVarmi(str));
+    }
+    private static boolean xyzVarmi(String str) {
+        if(str.contains("xyz")){
+            return true;
+        }else
+            return false;
+
+
+    }
+
+  
 }
