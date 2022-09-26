fffd899b6098e3341c15cb778b4fdbfd71a69c95
SunYasmin <110902506+SunYasmin@users.noreply.github.com>
2022-09-26 09:03:14 +0300

    Update C01_StringManipulation.java

--- src/p04_JavaPractise/C01_StringManipulation.java
+++ src/p04_JavaPractise/C01_StringManipulation.java
@@ -1,2 +1,32 @@
 package p04_JavaPractise;public class C01_StringManipulation {
+  /*
+     * Kulanicidan bir kelime isteyin eger kelime 3 ve daha fazla harfden olusuyorsa son
+     * iki harfini 3 kere yan yana yazdirin. degil ise girilen kelimeyi yazdirin
+     *
+     * ornek
+     * input = Ali
+     * output = lilili
+     *
+     * input = el
+     * output = el
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen isminizi yaziniz");
+        String isim = scan.nextLine();
+
+
+        if (isim.length()>=3){
+
+            System.out.println(isim.substring(isim.length()-2)+isim.substring(isim.length()-2)
+                    +isim.substring(isim.length()-2));
+
+        }else System.out.println(isim);
+
+
+
+    }
+
+  
+  
 }
