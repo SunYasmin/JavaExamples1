08d027f96fc0ffbb7e100232f102e7047a3641d9
SunYasmin <110902506+SunYasmin@users.noreply.github.com>
2022-09-26 09:00:48 +0300

    Update C05_NestedTernary.java

--- src/p03_JavaPractise/C05_NestedTernary.java
+++ src/p03_JavaPractise/C05_NestedTernary.java
@@ -1,2 +1,23 @@
 package p03_JavaPractise;public class C05_NestedTernary {
+  
+  public static void main(String[] args) {
+
+    /*
+    Final notu tanimlayin, Nested Ternary ile cozunuz
+    'A'  ->  "Gayet Basarili"
+    'B'  ->  "Basarili"
+    'C'  ->   "Ha gayret"
+    bu notlar disindakilere de Digerleri.. yazdiriniz
+     */
+        //sacnner yok burda
+
+        char finalNotu = 'B';
+
+        String result = finalNotu=='A' ? "Gayet Basarili" : finalNotu=='B' ? "Basarili" : finalNotu=='C' ? "Ha Gayret" : "Digerleri..";
+        // if            body si            if               body si         if              body          else
+        System.out.println("result : " + result);  // ust string result direk sout un icine yazilabilirrrr
+
+
+    }
+  
 }
