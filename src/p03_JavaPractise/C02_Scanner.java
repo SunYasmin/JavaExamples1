e266105130e55eb0ff0aebeb667c7f3a745f8546
SunYasmin <110902506+SunYasmin@users.noreply.github.com>
2022-09-26 08:58:41 +0300

    Update C02_Scanner.java

--- src/p03_JavaPractise/C02_Scanner.java
+++ src/p03_JavaPractise/C02_Scanner.java
@@ -1,2 +1,33 @@
 package p03_JavaPractise;public class C02_Scanner {
+  
+  public static void main(String[] args) {
+
+        /*  Problem Tanımı
+    Bir işçinin işi bitirme süresini ve toplam işçi sayısını alarak, işin bitme süresini
+    hesaplayan kodu yazınız.
+    Örneğin, Bir işçi bir işi 10 günde yapabilmektedir.
+    Buna göre 2 işçi aynı işi kaç günde yapar?
+
+    Örnek Ekran Çıktısı
+     Bir işçi işi kaç günde bitirmektedir? 10
+     Toplam kaç işçi çalışacak? 2
+    İşin bitme süresi 5 gündür.
+*/
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bir isci isi kac gunde bitirmektedir : ");
+
+
+        double birİsciBitirmeSuresi = scan.nextDouble();
+
+        System.out.println("Toplam kac kisi calisacak : ");
+        double isci_Sayisi = scan.nextDouble();
+
+        double cokluBitirmeSuresi = birİsciBitirmeSuresi/isci_Sayisi;
+        System.out.println("cokluBitirmeSuresi = " + cokluBitirmeSuresi);
+
+
+
+    }
+  
 }
