55db9b13656f39310bf523223c9c0e0122c99f44
SunYasmin <110902506+SunYasmin@users.noreply.github.com>
2022-09-26 09:03:36 +0300

    Update C02_StringManipulation.java

--- src/p04_JavaPractise/C02_StringManipulation.java
+++ src/p04_JavaPractise/C02_StringManipulation.java
@@ -1,2 +1,16 @@
 package p04_JavaPractise;public class C02_StringManipulation {
+  
+  public static void main(String[] args) {
+        //Kullanicidan ismini ve soyisimi girmesini isteyin, sonrasinda konsola tam ismini buyuk harfler ile yazdirin
+
+
+        Scanner scan  = new Scanner(System.in);
+        System.out.println("lutfen isminizi ve soy isminizi giriniz :");
+        String firstName = scan.nextLine(), lastName = scan.nextLine();  //multiple declaration
+
+        String fullName = firstName.concat(" " + lastName).toUpperCase();
+        System.out.println("fullName = " + fullName);
+
+    }
+  
 }
